80fe08b5b622946b2a2c43f0d8bcca91e78d6aa4
Jeremy Hart <jerome215@comcast.net>
2020-01-31 16:39:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -2,72 +2,84 @@
 def hopper
 	programmer_hash = 
  		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
+@@ -16,7 +15,7 @@ def hopper
         }
      }
 
-  programmer_hash[:grace_hopper]
 
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-
-
-	programmer_hash = 
-	   {:grace_hopper => {@@ -37,6 +38,7 @@ def alan_kay_is_known_for:languages => ["C"]}
+@@ -37,6 +36,7 @@ def alan_kay_is_known_for
+          :languages => ["C"]
+        }
      }
-     programmer_hash[:alan_kay][:known_for]
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
-  @@ -55,6 +57,7 @@ def dennis_ritchies_language
+@@ -55,6 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages][0]
+    programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
-@@ -77,7 +80,11 @@ def adding_matz
+@@ -66,7 +67,7 @@ def adding_matz
+
+	programmer_hash = 
+ 		{
+        :grace_hopper => {
+          :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+@@ -77,10 +78,12 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-        }
         },
-        :yukihiro_matsumoto => {
+          :yukihiro_matsumoto => {
           :known_for => "Ruby",
           :languages => ["LISP", "C"]
-       }
+        }
      }
 
 
-@@ -101,8 +108,8 @@ def changing_alan
+end
+
+def changing_alan
+@@ -99,8 +102,9 @@ def changing_alan
+          :languages => ["C"]
+        }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-
-
      programmer_hash[:alan_kay][:known_for] = alans_new_info
      return programmer_hash
-end
 
-def adding_to_dennis
-@@ -121,5 +128,6 @@ def adding_to_dennis
+
+end
+@@ -121,5 +125,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
 
-  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
-  programmer_hash
+     programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+     return programmer_hash
 end
+ ProTip! Use n and p to navigate between commits in a pull request.
+© 2020 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
